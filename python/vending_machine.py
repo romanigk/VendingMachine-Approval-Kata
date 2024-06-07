227b908279a2a6e62b500704db521e02147b32f8
Robert Manigk <105636975+romanigk@users.noreply.github.com>
2024-06-07 13:54:05 +0200

    Initialize empty_machine

---
+++ python/vending_machine.py
@@ -0,0 +1,10 @@
+
+class VendingMachine:
+    def __init__(self):
+        self.coins = []
+        self.balance = 0
+        self.display = "INSERT COIN"
+
+    def insert_coin(self, value):
+        # TODO: write this code
+        pass
