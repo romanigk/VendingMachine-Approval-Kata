bc70c205e0d73d4a98c4ffceeef7676ed25d8213
Robert Manigk <105636975+romanigk@users.noreply.github.com>
2024-06-07 13:54:07 +0200

    Initialize demo_dave

---
+++ csharp/VendingMachine-Approval-Kata/VendingMachine.cs
@@ -0,0 +1,50 @@
+﻿using System.Globalization;
+
+namespace VendingMachine_Approval_Kata;
+
+public class VendingMachine
+{
+    public List<int> Coins { get; } = new();
+    public string Display { get; private set; }
+    public int Balance { get; private set; }
+
+    public List<int> Returns { get; private set; } = new();
+
+
+    private readonly CultureInfo _en_Us_Culture;
+
+    public VendingMachine()
+    {
+        Display = "";
+        _en_Us_Culture = CultureInfo.CreateSpecificCulture("en-US");
+
+        DisplayBalance();
+    }
+
+    private void DisplayBalance()
+    {
+        Display = Balance != 0 ? FormatAsDollars(Balance) : "INSERT COIN";
+    }
+
+    private string FormatAsDollars(int cents)
+    {
+        return (cents / 100.0).ToString("C", _en_Us_Culture);
+    }
+
+    public void InsertCoin(int coin)
+    {
+        var acceptedCoins = new List<int>() { 5, 10, 25 };
+
+        if (acceptedCoins.Contains(coin))
+        {
+            Coins.Add(coin);
+            Balance += coin;
+            DisplayBalance();
+        }
+        else
+        {
+            Returns.Add(coin);
+        }
+    }
+
+}
